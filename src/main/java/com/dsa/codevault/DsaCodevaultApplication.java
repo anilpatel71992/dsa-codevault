7b67b59513c1e5671c75b0f75a3772f6256ff834
anil.email <rsanuppatel@gmail.com>
2025-08-15 16:38:50 +0530

    Initial commit

---
+++ src/main/java/com/dsa/codevault/DsaCodevaultApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.codevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaCodevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaCodevaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsa/codevault/DsaCodevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.codevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaCodevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
