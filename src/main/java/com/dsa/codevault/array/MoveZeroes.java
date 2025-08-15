47d8b6b8605b03bec8fedcdde27879972dcc9c94
anil.email <rsanuppatel@gmail.com>
2025-08-15 17:15:04 +0530

    Array: LC-283 Move Zeroes

--- src/main/java/com/dsa/codevault/array/MoveZeroes.java
+++ src/main/java/com/dsa/codevault/array/MoveZeroes.java
@@ -1,7 +1,3 @@
-package com.dsa.codevault.array;
-
-import java.util.Arrays;
-
 /*
 	Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 	Note that you must do this in-place without making a copy of the array.
@@ -17,6 +13,8 @@ import java.util.Arrays;
 	1 <= nums.length <= 104
 	-231 <= nums[i] <= 231 - 1
 */
+package com.dsa.codevault.array;
+import java.util.Arrays;
 public class MoveZeroes {
 
 	public static void main(String[] args) {
